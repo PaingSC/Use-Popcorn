5c9158295defcf4a3c7ee8cbd8ff8e0e0336bb7a
PaingSC <paingsoechit.job@gmail.com>
2024-01-28 01:09:58 +0630

    Updating:Component:StarRating-practice: adding more functionalities

--- src/index copy.js
+++ src/index copy.js
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import StarRating from "./StarRating-practice";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <StarRating maxRating={3} />
     <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      size={25}
+      color="aqua"
+    />
+    <StarRating
+      maxRating={3}
+      messages={["Bad", "Okay", "Good"]}
       color="blue"
       size={30}
-      className="rating-style"
+      className="star-rating"
     />
-    <StarRating maxRating={10} color="red" size={40} className="test" />
   </React.StrictMode>
 );
--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
-import StarRating from "./StarRating-practice";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <StarRating
+    <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       color="blue"
       size={30}
       className="rating-style"
-    /> */}
-    <StarRating maxRating={3} />
-    <StarRating maxRating={3} messages={["Bad", "Okay", "Good"]} />
+    />
+    <StarRating maxRating={10} color="red" size={40} className="test" />
   </React.StrictMode>
 );
