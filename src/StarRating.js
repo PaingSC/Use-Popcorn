c2decc76e74abf1a86d07f117afbe0f5f8402807
PaingSC <paingsoechit.job@gmail.com>
2024-01-28 01:33:20 +0630

    Updating: Making external state updatable inside in the component

--- src/StarRating.js
+++ src/StarRating.js
@@ -17,9 +17,15 @@ export default function StarRating({
   size = 30,
   className = "",
   messages = [],
+  onSetRating,
 }) {
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating) {
+    setRating(rating);
+    onSetRating(rating);
+  }
 
   // CSS
   const textStyle = {
@@ -37,7 +43,7 @@ export default function StarRating({
           {Array.from({ length: maxRating }, (_, i) => (
             <Star
               key={i}
-              onRate={() => setRating(i + 1)}
+              onRate={() => handleRating(i + 1)}
               full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
               onHoverEnter={() => setTempRating(i + 1)}
               onHoverLeave={() => setTempRating(0)}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <>
+      <StarRating onSetRating={setMovieRating} />
+      <p>This movie is rated {movieRating} stars.</p>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,5 +25,6 @@ root.render(
       className="rating-style"
     />
     <StarRating maxRating={10} color="red" size={40} className="test" />
+    <Test />
   </React.StrictMode>
 );
